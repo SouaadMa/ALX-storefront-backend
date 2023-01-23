14314eb1f4da8aa76c1e0fa8d06f8d3641c6070c
Benelhadj Dj. Mama Saadia <51034305+SouaadMa@users.noreply.github.com>
2023-01-23 22:56:03 +0100

    Update 20230121060403-users-table-up.sql

--- migrations/sqls/20230121060403-users-table-up.sql
+++ migrations/sqls/20230121060403-users-table-up.sql
@@ -1,6 +1,10 @@
+DROP TABLE IF EXISTS order_products;
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS products;
 CREATE TABLE users (
     id SERIAL PRIMARY  KEY,
-    firstName VARCHAR,
-    lastName VARCHAR,
+    firstname VARCHAR,
+    lastname VARCHAR,
     password VARCHAR
 );
